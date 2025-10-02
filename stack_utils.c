ce5f94ec4db6e5a0a7f35c0a49960354a0e41ad3
Lorenzo Yanga <lorenzoyanga@gmail.com>
2025-10-02 11:21:04 +0800

    fix critical bug (how did this not get spotted)

--- stack_utils.c
+++ stack_utils.c
@@ -6,12 +6,11 @@
 /*   By: lyanga <lyanga@student.42singapore.sg>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/18 15:18:07 by lyanga            #+#    #+#             */
-/*   Updated: 2025/09/23 09:01:27 by lyanga           ###   ########.fr       */
+/*   Updated: 2025/10/02 11:20:29 by lyanga           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-
 void	stack_del(t_stack *head)
 {
 	t_stack	*tmp;
@@ -53,7 +52,7 @@ int	stack_biggest(t_stack *head)
 	biggest = -1;
 	while (head)
 	{
-		if (head->value > biggest)
+		if (head->index > biggest)
 			biggest = head->index;
 		head = head->next;
 	}
